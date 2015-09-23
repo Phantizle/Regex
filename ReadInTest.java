670069666ea19d22507b352edd5411c95ca4f908
Phantizle <Phantizle@users.noreply.github.com>
2015-09-23 19:53:13 -0400

    Create ReadInTest.java

---
+++ ReadInTest.java
@@ -0,0 +1,15 @@
+package Class;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReadInTest {
+
+	@Test
+	public void testMain() {
+		assertEquals("", "");
+		fail("Not yet implemented");
+	}
+
+}
